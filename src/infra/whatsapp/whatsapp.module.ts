c185c5ab0f4eb8445a133db4ed74a5fa2aafed3d
gregoritrentin <gregori@prosperatecnologia.com.br>
2024-12-12 18:02:17 -0300

    ajuste no modulo do wpp

--- src/infra/whatsapp/whatsapp.module.ts
+++ src/infra/whatsapp/whatsapp.module.ts
@@ -1,8 +1,6 @@
-// src/infra/whatsapp/whatsapp.module.ts
-
 import { WhatsAppProvider } from '@/domain/interfaces/whatsapp-provider'
 import { Module } from '@nestjs/common'
-import { WhatsAppService } from '@/infra/whatsApp/whatsapp.service'
+import { WhatsAppService } from '@/infra/whatsapp/whatsapp.service'
 import { EnvModule } from '../env/env.module'
 import { DatabaseModule } from '../database/database.module'
 import { FileModule } from '../file/file.module'
